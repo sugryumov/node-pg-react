8d893d10ff217ccddd38c0ba9013415e90f4e632
Sergey Ugryumov <snugryumov@gmail.com>
2022-09-16 10:22:42 +0300

    feature(auth-front): add interceptor response

--- client/src/services/apiService.ts
+++ client/src/services/apiService.ts
@@ -1,10 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { RootState } from "../store";
+import { IAuthResponse } from "../models/response/IAuthResponse";
+import { authActions } from "../store/reducers/authSlice";
 
 const baseQuery = fetchBaseQuery({
   credentials: "include",
   baseUrl: "http://localhost:9000/api",
-  prepareHeaders: (headers, { getState }) => {
+  prepareHeaders: (headers) => {
     const token = localStorage.getItem("token");
 
     if (token) {
@@ -18,17 +19,17 @@ const baseQuery = fetchBaseQuery({
 const baseQueryWithReAuth = async (args: any, api: any, extraOptions: any) => {
   let result = await baseQuery(args, api, extraOptions);
 
-  if (result.error && result.error.status === 401) {
-    // try to get a new token
-    // const refreshResult = await baseQuery("/refreshToken", api, extraOptions);
-    // if (refreshResult.data) {
-    //   // store the new token
-    //   api.dispatch(tokenReceived(refreshResult.data));
-    //   // retry the initial query
-    //   result = await baseQuery(args, api, extraOptions);
-    // } else {
-    //   api.dispatch(loggedOut());
-    // }
+  if (result?.error?.status === 401) {
+    const response = await baseQuery("/refresh", api, extraOptions);
+
+    if (response?.data) {
+      const { accessToken } = response.data as IAuthResponse;
+      localStorage.setItem("token", accessToken);
+
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(authActions.logout());
+    }
   }
   return result;
 };
